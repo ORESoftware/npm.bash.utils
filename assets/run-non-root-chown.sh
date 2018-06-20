ab06209d4fb3affaeab0b03818f8888d9e74e4c1
Olegzandr Denman <alex@oresoftware.com>
2018-06-19 18:53:36 -0700

    pdev:'set'

---
+++ assets/run-non-root-chown.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+
+if [ "$(id -u)" -ne "0" ]; then
+    # The root user has UID 0 (regardless of the name of the "root" account)
+    sudo chown -R $(whoami) $(npm config get prefix)/lib
+    sudo chown -R $(whoami) $(npm config get prefix)/lib/node_modules
+    sudo chown -R $(whoami) $(npm config get prefix)/bin
+    sudo chown -R $(whoami) $(npm config get prefix)/share
+    sudo chown -R $(whoami) /usr/local/lib
+    sudo chown -R $(whoami) /usr/local/etc
+fi
+
+
+
---
+++ assets/run-npm-config-settings.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+npm set loglevel=warn
+npm set unsafe-perm true
+npm set cache-min 9999999
+npm set progress=false
