9989869d90ec2e82f2464c66ed8a2184f6daf600
Olegzandr Denman <alex@oresoftware.com>
2018-06-19 22:44:04 -0700

    pdev:'set'

--- assets/run-npm-config-settings.sh
+++ assets/run-npm-config-settings.sh
@@ -6,3 +6,8 @@ npm set loglevel=warn
 npm set unsafe-perm true
 npm set cache-min 9999999
 npm set progress=false
+
+npm config set loglevel=warn
+npm config set unsafe-perm true
+npm config set cache-min 9999999
+npm config set progress=false
